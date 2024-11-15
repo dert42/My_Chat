d3710625de8865ac4834147db34e91396f35f724
IvanProhorov <ivan-prokhorov-04@mail.ru>
2024-11-15 22:21:57 +0300

    login and register

--- ChatApp/consumers.py
+++ ChatApp/consumers.py
@@ -1,26 +1,19 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
+from django.utils import timezone
 from .models import DB_Message
-
+from asgiref.sync import sync_to_async
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        # Добавление клиента в общую группу 'chat'
-        await self.channel_layer.group_add(
-            'chat',  # Убедитесь, что группа называется 'chat'
-            self.channel_name
-        )
-
+        self.user = self.scope['user']
+        await self.channel_layer.group_add('chat', self.channel_name)
         await self.accept()
         await self.load_messages()  # Загрузка сообщений при подключении
 
     async def disconnect(self, close_code):
-        # Удаление клиента из группы при отключении
-        await self.channel_layer.group_discard(
-            'chat',  # Убедитесь, что группа называется 'chat'
-            self.channel_name
-        )
+        await self.channel_layer.group_discard('chat', self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -28,28 +21,45 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.broadcast(data['message'])
 
     async def save_message(self, content):
-        message = DB_Message(content=content)
+        now = timezone.now()
+        message = DB_Message(content=content,
+                             timestamp=now,
+                             user=self.user)
         await database_sync_to_async(message.save)()  # Асинхронное сохранение сообщения
 
     async def load_messages(self):
-        messages = await self.get_messages()  # Получить сообщения асинхронно
+        messages = await self.get_messages()  # Get messages directly
         for message in messages:
-            await self.send(text_data=json.dumps({'message': message.content}))
+            username = await sync_to_async(lambda: message.user.username)()
+            timestamp = str(await sync_to_async(lambda: message.timestamp)())
+            await self.send(text_data=json.dumps({
+                'message': message.content,
+                'user': username,  # Username
+                'datetime': timestamp,  # Message timestamp
+            }))
 
     async def broadcast(self, message):
-        # Асинхронная отправка сообщения всем подключенным клиентам
-        await self.channel_layer.group_send(
-            'chat',  # Используем имя группы 'chat'
-            {
-                'type': 'chat_message',
-                'message': message
+        now = timezone.now()
+        username = self.user.username  # Получаем имя пользователя
+        await self.channel_layer.group_send('chat',
+            {   'type': 'chat_message',
+                'message': message,
+                'user': username,
+                'datetime': now.isoformat(),
             }
         )
 
     async def chat_message(self, event):
         message = event['message']
-        await self.send(text_data=json.dumps({'message': message}))
+        user = event['user']
+        await self.send(text_data=json.dumps({
+            'type': 'chat_message',
+            'message': message,
+            'user': user,
+            'datetime': event['datetime'],
+        }))
 
     @database_sync_to_async
     def get_messages(self):
-        return list(DB_Message.objects.all())  # Извлечение всех сообщений из базы данных и преобразование в список
+        return list(DB_Message.objects.all())  # Извлечение сообщений для конкретной комнаты
+
--- ChatApp/models.py
+++ ChatApp/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class DB_Message(models.Model):
     content = models.TextField()  # Содержимое сообщения
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()  # Содержимое сообщения
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def str(self):
+        return f"{self.user.username}: {self.content}"
